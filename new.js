a039de796aed7ad36789a798e08e1b5ab5644f32
Bohdan <bohdanholubn@gmail.com>
2022-10-30 13:20:56 +0300

    first commit

---
+++ new.js
@@ -0,0 +1,96 @@
+// "use strict";
+
+// // let number = 4.6;
+
+// // console.log(4/0);
+// // console.log('string' * 9);
+
+// // const persone = "Alex";
+
+// // const bool = false;
+
+// // console.log(something);
+
+// // let und;
+// // console.log(und);
+
+// // const obj = {
+// //     name: "John",
+// //     age: 25,
+// //     isMarried: false
+// // };
+
+// // // console.log(obj.name);
+// // console.log(obj['name']);
+
+// // let arr = ['plum.png', 'orange.jpg', 6, 'apple.bmp', {}, []];
+// // console.log(arr[0]);
+
+// // const arr = ['a', 'b', 'c'];
+
+// // arr[10] = '3456';
+
+// // console.log(arr);
+
+// // const arrObj = {
+// //     0: 'a',
+// //     1: 'b',
+// //     2: 'c',
+// //     abc: {
+// //         def: {
+            
+// //         }
+// //     }
+// // };
+// // const b = 'b';
+
+// // // arrObj.b = '1234';
+// // arrObj['b'] = '1234';
+
+
+// // console.log(arrObj['b']);
+// // console.log(arrObj.b);
+
+
+// // // const obj = {a:1, b:2};
+// // const obj = {
+// //     "Anna": 500,
+// //     "Alice": 800
+// // };
+
+// // console.log(obj);
+
+// // Homework_
+// const storeName = 'MarketPlace';
+// let storeDescrition = {
+//     budget: 10000,
+//     employees: ['Artur', 'Katarina', 'Stiv'],
+//     products: {
+//         sigaretes: 100,
+//         alcohol: 200
+//     },
+//     open: true,
+// };
+
+// console.log(storeName);
+// console.log(storeDescrition);
+
+// console.log('arr' + " - object");
+// console.log(4 + "3");
+
+// let incr = 10,
+//     decr = 10;
+
+// // ++incr;
+// // --decr;
+
+// console.log(incr++);
+// console.log(decr--);
+
+// console.log(5%2);
+
+// console.log(2*4 === '8');
+
+// const isChecked = true,
+//       isClose = false;
+// console.log(isChecked && isClose);
